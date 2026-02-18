2409c0ea52b9ec808332bae6867066b0807adccb
Andy Phan <47342162+xapzx@users.noreply.github.com>
2026-02-18 04:44:30 +0000

    pipeline

---
+++ pipeline/pipeline.py
@@ -0,0 +1,12 @@
+import sys
+
+import pandas as pd
+
+month = int(sys.argv[1])
+
+df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
+print(df.head())
+
+df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
+
+print(f'hello pipeline, month={month}')
